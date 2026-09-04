9200949e12bce66fa185591bcf7f594768eddf50
LHBUSA <sales@localhomebuyersusa.com>
2026-09-04 17:58:59 -0500

    Build GB homepage docs and workspace

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        docs: resolve(__dirname, 'docs/index.html'),
+        workspace: resolve(__dirname, 'workspace/index.html'),
+      },
+    },
   },
 });
